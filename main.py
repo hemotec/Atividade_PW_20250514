2bfe116ddf99d64ee8f530eef21fed1b921d00a4
Cauã Gomes Marvila <caua.privg@gmail.com>
2025-05-14 18:00:51 -0300

    primeira etapa concluida

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ templates = Jinja2Templates(directory="templates")
 produto_repo.criar_tabela()
 cliente_repo.criar_tabela()
 
-
 @app.get("/")
 async def get_root():
     produtos = produto_repo.obter_todos()
@@ -28,6 +27,11 @@ async def get_clientes():
     response = templates.TemplateResponse("clientes.html", {"request": {}, "clientes": clientes})
     return response
 
+@app.get("/produtos")
+async def get_produtos():
+    produtos = produto_repo.obter_todos()
+    response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
+    return response
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
